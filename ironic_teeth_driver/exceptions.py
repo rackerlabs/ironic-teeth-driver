9e606ad35fe75009a27f97b1fc0f8d568ce51d1b
Josh Gachnang <josh.gachnang@rackspace.com>
2014-02-14 11:36:08 -0800

    Use Ironic exceptions and json

--- ironic_teeth_driver/exceptions.py
+++ ironic_teeth_driver/exceptions.py
@@ -16,20 +16,21 @@ limitations under the License.
 
 import collections
 
-from ironic_teeth_driver import encoding
+from ironic.common import exception
+from ironic.openstack.common.gettextutils import _
 
 
-class RESTError(Exception, encoding.Serializable):
+class RESTError(exception.IronicException):
     """Base class for errors generated in teeth."""
-    message = 'An error occurred'
-    details = 'An unexpected error occurred. Please try back later.'
-    status_code = 500
+    message = _('An error occurred')
+    code = 500
+    details = _('An unexpected error occurred. Please try back later.')
 
-    def serialize(self, view):
+    def serialize(self):
         """Turn a RESTError into a dict."""
         return collections.OrderedDict([
             ('type', self.__class__.__name__),
-            ('code', self.status_code),
+            ('code', self.code),
             ('message', self.message),
             ('details', self.details),
         ])
@@ -39,12 +40,12 @@ class UnsupportedContentTypeError(RESTError):
     """Error which occurs when a user supplies an unsupported
     `Content-Type` (ie, anything other than `application/json`).
     """
-    message = 'Unsupported Content-Type'
-    status_code = 400
+    message = _('Unsupported Content-Type')
+    code = 400
 
     def __init__(self, content_type):
         self.details = 'Content-Type "{content_type}" is not supported'
-        self.details = self.details.format(content_type=content_type)
+        self.details = _(self.details.format(content_type=content_type))
 
 
 class InvalidContentError(RESTError):
@@ -52,11 +53,11 @@ class InvalidContentError(RESTError):
     because that content cannot be parsed according to the advertised
     `Content-Type`, or due to a content validation error.
     """
-    message = 'Invalid request body'
-    status_code = 400
+    message = _('Invalid request body')
+    code = 400
 
     def __init__(self, details):
-        self.details = details
+        self.details = _(details)
 
 
 class NotFound(RESTError):
@@ -64,9 +65,9 @@ class NotFound(RESTError):
     because that content cannot be parsed according to the advertised
     `Content-Type`, or due to a content validation error.
     """
-    message = 'Not found'
-    status_code = 404
-    details = 'The requested URL was not found.'
+    message = _('Not found')
+    code = 404
+    details = _('The requested URL was not found.')
 
 
 class ChassisAlreadyReservedError(RESTError):
@@ -74,19 +75,19 @@ class ChassisAlreadyReservedError(RESTError):
     someone else reserves it first. This should generally be handled
     by requesting another chassis from the scheduler.
     """
-    message = 'Chassis already reserved'
+    message = _('Chassis already reserved')
 
     def __init__(self, chassis):
-        self.details = 'Chassis {chassis_id} is already reserved.'.format(
-            chassis_id=str(chassis.id))
+        self.details = _('Chassis {chassis_id} is already reserved.'.format(
+            chassis_id=str(chassis.id)))
 
 
 class InsufficientCapacityError(RESTError):
     """Error which occurs when not enough capacity is available to
     fulfill a request.
     """
-    message = 'Insufficient capacity'
-    details = ('There was not enough capacity available to fulfill your '
+    message = _('Insufficient capacity')
+    details = _('There was not enough capacity available to fulfill your '
                'request. Please try back later.')
 
 
@@ -94,59 +95,60 @@ class AgentNotConnectedError(RESTError):
     """Error which occurs when an RPC call is attempted against a chassis
     for which no agent is connected.
     """
-    message = 'Agent not connected'
+    message = _('Agent not connected')
 
     def __init__(self, chassis_id):
         self.details = 'No agent is connected for chassis {chassis_id}'
-        self.details = self.details.format(chassis_id=chassis_id)
+        self.details = _(self.details.format(chassis_id=chassis_id))
 
 
 class AgentConnectionLostError(RESTError):
     """Error which occurs when an agent's connection is lsot while an RPC
     call is in progress.
     """
-    message = 'Agent connection lost'
-    details = 'The agent\'s connection was lost while performing your request.'
+    message = _('Agent connection lost')
+    details = _('The agent\'s connection was lost while performing your ' \
+                 'request.')
 
 
 class AgentExecutionError(RESTError):
     """Exception class which represents errors that occurred in the agent."""
-    message = 'Error executing command'
+    message = _('Error executing command')
 
     def __init__(self, details):
-        self.details = details
+        self.details = _(details)
 
 
 class ImageNotFoundError(InvalidContentError):
     """Error which is raised when an image is not found."""
-    message = 'Image not found'
+    message = _('Image not found')
 
     def __init__(self, image_id):
-        msg = 'Image "{image_id}" not found.'.format(image_id=str(image_id))
+        msg = _('Image "{image_id}" not found.'.format(image_id=str(image_id)))
         super(ImageNotFoundError, self).__init__(msg)
 
 
 class InvalidParametersError(RESTError):
     """Error which is raised for invalid parameters."""
-    message = 'Invalid query parameters'
-    status_code = 400
+    message = _('Invalid query parameters')
+    code = 400
 
     def __init__(self, details):
-        self.details = details
+        self.details = _(details)
 
 
 class ObjectCannotBeDeletedError(RESTError):
     """Error which is raised when a consumer attempts to delete an
     objects which can't be deleted. (ex: foreign key constraint)
     """
-    message = 'Object cannot be deleted'
-    status_code = 403
+    message = _('Object cannot be deleted')
+    code = 403
 
     def __init__(self, cls, id, details=None):
         super(ObjectCannotBeDeletedError, self).__init__(cls, id)
         default_details = '{type} with id {id} cannot be deleted.'.format(
             type=cls.__name__, id=id)
-        self.details = details if details else default_details
+        self.details = _(details if details else default_details)
 
 
 class ObjectAlreadyDeletedError(RESTError):
@@ -157,33 +159,33 @@ class ObjectAlreadyDeletedError(RESTError):
     states are indistinguishable to the system) a
     `RequestedObjectNotFoundError` should be used instead.
     """
-    message = 'Object already deleted'
-    status_code = 403
+    message = _('Object already deleted')
+    code = 403
 
     def __init__(self, cls, id):
         super(ObjectAlreadyDeletedError, self).__init__(cls, id)
-        self.details = '{type} with id {id} has already been deleted.'.format(
-            type=cls.__name__, id=id)
+        self.details = _('{type} with id {id} has already been deleted.'
+                         .format(type=cls.__name__, id=id))
 
 
 class RequestedObjectNotFoundError(RESTError):
     """Error which is returned when a requested object is not found."""
-    message = 'Requested object not found'
-    status_code = 404
+    message = _('Requested object not found')
+    code = 404
 
     def __init__(self, cls, id):
         super(RequestedObjectNotFoundError, self).__init__(cls, id)
-        self.details = '{type} with id {id} not found.'.format(
-            type=cls.__name__, id=id)
+        self.details = _('{type} with id {id} not found.'.format(
+            type=cls.__name__, id=id))
 
 
 class MultipleChassisFound(RESTError):
     """Error when chassis lookup is done by hardware keys,
     and more than one matching chassis is found.
     """
-    message = 'Multiple chassis found'
-    status_code = 400
+    message = _('Multiple chassis found')
+    code = 400
 
     def __init__(self, hardware):
         self.details = 'Multiple chassis with hardware {} were found.'
-        self.details = self.details.format(hardware)
+        self.details = _(self.details.format(hardware))
